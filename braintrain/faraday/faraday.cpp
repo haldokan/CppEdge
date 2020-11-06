d08b614d7a3aa66cf430fcbe016760402c73d182
haldokanji <haldokanji@bloomberg.net>
2020-11-05 23:13:17 -0500

    start that thread about threading needles!

---
+++ braintrain/faraday/faraday.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+using namespace std;
+
+// note that argc will always be 2 or more; first custom arg is at index 2
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
