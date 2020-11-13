d149bbcec65ce6b4426fb9b04fecfa3d23e47d74
haldokanji <haldokanji@bloomberg.net>
2020-11-12 21:14:00 -0500

    named params

--- braintrain/boostdi/bindings/bindings1.cpp
+++ braintrain/boostdi/bindings/bindings1.cpp
@@ -1,21 +1,31 @@
+#include <iostream>
 #include "../di.hpp"
 
 using namespace std;
 namespace di = boost::di;
 
 struct renderer {
-    string device;
+    int device;
 };
 
 class view {
 public:
-    view(string name,renderer& renderer): m_name{std::move(name)}, m_renderer {renderer} {}
+    view(string name,renderer& renderer): m_name{std::move(name)}, m_renderer {renderer} { cout << "r.device: " << renderer.device <<endl;}
 private:
     renderer m_renderer;
     string m_name;
 };
 
-struct model {};
+auto Size = []{}; //we noop lambda for the param name - in a real app this would have to be put in an h file and imported to using code?
+struct model {
+public:
+    // boost:di chooses the cntr with the most args. But with cntrs having the same num of args we need to tell boost:di which cntr we want to use
+    model(int height, int width) {cout << "height: " << height << ", width: " << width << endl;}
+    // indicating what cntr to use is done using the macro below. Another way is to add ... as the last arg in the cntr
+    // signature: model(int size, double precision, ...) {}
+    BOOST_DI_INJECT(model, (named = Size) int size, double precision); // we separate the cntr declaration form definition to show that named params need apply only to the declaration
+};
+model::model(int size, double precision) {cout << "size: " << size << ", precision: " << precision << endl;}
 
 class controller {
 public:
@@ -36,7 +46,11 @@ private:
 };
 
 int main() {
-    // just like that! it creates the dependency graph
-    app app1 = di::make_injector().create<app>();
+    app app1 = di::make_injector(
+            di::bind<int>.to(17),
+            // this will insure that the int bind in the line above is applied to the renderer.device only
+            di::bind<int>.named(Size).to(13), // more syntax acrobatics
+            di::bind<double>.to(23.25)
+    ).create<app>();
     return 0;
 }
---
+++ braintrain/boostdi/bindings/bindings5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "../di.hpp"
+
+using namespace std;
+namespace di = boost::di;
+
+void split_configs() {
+    cout << "split_configs" << endl;
+}
+
+int main() {
+    split_configs();
+
+    return 0;
+}
+
+
